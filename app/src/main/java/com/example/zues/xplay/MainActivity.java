17d370b56bfb41679b21faf1be8111b972aa5496
greatzues <greatzly@outlook.com>
2016-04-05 12:00:44 +0800

    完成home部分布局

--- app/src/main/java/com/example/zues/xplay/MainActivity.java
+++ app/src/main/java/com/example/zues/xplay/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.zues.xplay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 }
